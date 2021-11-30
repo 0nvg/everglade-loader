d8577c3dafb78b9469d519dd1603b0b7a25e9d65
aeg <93492434+aeg-eus@users.noreply.github.com>
2021-11-30 15:05:52 +0300

    Interface Changes
    fixed dex explorer and used the light one instead

--- hub.lua
+++ hub.lua
@@ -1,5 +1,5 @@
 -- by avg
--- ver 1.4
+-- ver 1.5
 
 local theeverglade = Instance.new("ScreenGui")
 local header = Instance.new("Frame")
@@ -146,7 +146,7 @@ Test4.BorderSizePixel = 0
 Test4.Position = UDim2.new(0, 37, 0, 129)
 Test4.Size = UDim2.new(0, 125, 0, 30)
 Test4.Font = Enum.Font.SourceSansItalic
-Test4.Text = "Dark Dex"
+Test4.Text = "Dex"
 Test4.TextColor3 = Color3.fromRGB(0, 0, 0)
 Test4.TextScaled = true
 Test4.TextSize = 14.000
@@ -622,85 +622,7 @@ local function TJVQI_fake_script()
 	end)
 	
 	t4.MouseButton1Click:Connect(function()
-		-- < Services > --
-		local InsertService = game:GetService("InsertService")
-		local CoreGui = game:GetService("CoreGui")
-		-- < Aliases > --
-		local table_insert = table.insert
-		local table_foreach = table.foreach
-		local string_char = string.char
-		local getobjects = function(a) -- Faster than game:GetObjects(a)
-			local Objects = {}
-			if a then
-				local b = InsertService:LoadLocalAsset(a)
-				if b then 
-					table_insert(Objects, b) 
-				end
-			end
-			return Objects
-		end
-		-- < Values > --
-		local Charset = {}
-		local Random_Instance = Random.new()
-		local RemoteDebugWindow = CoreGui:FindFirstChild("RemoteDebugWindow", true)
-		-- < Source > --
-		if RemoteDebugWindow then
-			RemoteDebugWindow.Parent:Destroy()
-		end
-
-		for i = 48,  57 do 
-			table_insert(Charset, string_char(i))
-		end
-
-		for i = 65,  90 do 
-			table_insert(Charset, string_char(i))
-		end
-
-		for i = 97, 122 do
-			table_insert(Charset, string_char(i))
-		end
-
-		function RandomCharacters(length)
-			return length > 0 and RandomCharacters(length - 1)..Charset[Random_Instance:NextInteger(1, #Charset)] or ""
-		end
-
-		-- Main version : "rbxassetid://5475777257"
-		local Dex = getobjects("rbxassetid://5475777257")[1] -- Beta version: "rbxassetid://5482100934" | I will constantly update beta version so it might break
-		pcall(syn.protect_gui, Dex)
-		Dex.Name = RandomCharacters(Random_Instance:NextInteger(5,20))
-		Dex.Parent = CoreGui
-
-		local function Load(Obj, Url)
-			local function GiveOwnGlobals(Func, Script)
-				local Fenv, RealFenv, FenvMt = {}, {script = Script}, {}
-				FenvMt.__index = function(a,b)
-					return RealFenv[b] == nil and getgenv()[b] or RealFenv[b]
-				end
-				FenvMt.__newindex = function(a, b, c)
-					if RealFenv[b] == nil then 
-						getgenv()[b] = c 
-					else 
-						RealFenv[b] = c 
-					end
-				end
-				setmetatable(Fenv, FenvMt)
-				pcall(setfenv, Func, Fenv)
-				return Func
-			end
-
-			local function LoadScripts(_, Script)
-				if Script:IsA("LocalScript") then
-					spawn(function()
-						GiveOwnGlobals(loadstring(Script.Source,"="..Script:GetFullName()), Script)()
-					end)
-				end
-				table_foreach(Script:GetChildren(), LoadScripts)
-			end
-
-			LoadScripts(nil, Obj)
-		end
-
-		Load(Dex)
+		loadstring(game:GetObjects("rbxassetid://418957341")[1].Source)()
 		print(t4load)
 	end)
 	
